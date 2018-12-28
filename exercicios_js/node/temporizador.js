82ce0724417818ae18853800766da6f26f3453a0
HemersonRafael <rafaelpontes1995@gmail.com>
2018-12-28 02:06:04 -0300

    add folder ESNext

---
+++ exercicios_js/node/temporizador.js
@@ -0,0 +1,19 @@
+const schedule = require('node-schedule')
+
+const tarefa1 = schedule.scheduleJob('* * * * * * *', function (){
+    console.log('Executando Tarefa1!', new Date().getSeconds())
+})
+console.log(new Date().getHours())
+setTimeout(function(){
+    tarefa1.cancel()
+    console.log('cancelando tafera1')
+},20000)
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1,5)]
+regra.hour = 1
+regra.second = 30
+
+const tarefa2 = schedule.scheduleJob(regra, function(){
+    console.log('executando tarefa 2!',new Date().getSeconds())
+})
