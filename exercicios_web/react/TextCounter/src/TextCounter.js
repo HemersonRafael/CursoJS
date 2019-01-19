7889d47c2d75d649b55f4c59d1b3c3baf1dc125d
HemersonRafael <rafaelpontes1995@gmail.com>
2019-01-19 01:36:30 -0300

    update folder react

--- exercicios_web/react/TextCounter/src/TextCounter.js
+++ exercicios_web/react/TextCounter/src/TextCounter.js
@@ -3,22 +3,37 @@ import React, { Component } from 'react';
 
 class TextCounter extends Component{
     static defaultProps = {
-        limit: 15
+        limit: 15,
     }
 
     constructor(props){
         super(props);
         this.state = {
-            totalChars: 0
+            totalChars: 0,
+            text: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        const element = event.target
+        const text = element.value
+        if( text.length <= this.props.limit){
+            this.setState(
+                {
+                    totalChars: text.length, 
+                    text: text
+                }
+            )
         }
     }
 
     render(){
-        const {state, props} = this;
+        const {state, props} = this
         return (
             <div>
                 <h1>Meu Contador</h1>
-                <textarea />
+                <textarea onChange={this.handleChange} value = {state.text}></textarea>
                 <div>
                     <strong>Total:</strong> { state.totalChars } / { props.limit }
                 </div>
